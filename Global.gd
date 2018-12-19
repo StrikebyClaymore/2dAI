c3cb25d1e323d9d0b07a93643cfcc3a7f71a51bb
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2018-12-19 10:00:51 +0300

    Update Global.gd

--- Global.gd
+++ Global.gd
@@ -6,10 +6,11 @@ onready var pathfinder_node = current_scene.get_node("Pathfinder")
 onready var player = current_scene.get_node("Player")
 
 var cell = null
-var blocked = false
+var locked = false
 
 var flag = false
 
+var closedCells = []
 var enemies_paths = []
 
 func _ready():
@@ -18,12 +19,12 @@ func _ready():
 
 func UnlockEnemies():
 	for e in get_tree().get_nodes_in_group("enemy"):
-		e.timer.start()
-		e.target_position = player.position
-		e.life = true
-		e.cell = e.cell
-		e.ChangeState(e.IDLE)
-
+		#e.timer.start()
+		e.target = cell
+		#closedCells.clear()
+		#e.life = true
+		#e.cell = e.cell
+		#e.ChangeState(e.IDLE)
 
 func CheckPath(enemy):
 	for e in get_tree().get_nodes_in_group("enemy"):
@@ -35,23 +36,43 @@ func CheckPath(enemy):
 			return false
 	return true
 
-func CheckBlocked():
-	var pos = pathfinder_node.GlobalToPoint(cell.position)
+func CheckBlocked(obj, flag = false):
+	var pos 
+	if typeof(obj) != TYPE_VECTOR2:
+		pos = pathfinder_node.GlobalToPoint(obj.position)
+	else:
+		pos = obj
 	var points = PoolVector2Array([pos + Vector2(-1, 0), pos + Vector2(1, 0),
 								pos + Vector2(0, -1), pos + Vector2(0, 1),
 								pos + Vector2(-1, -1), pos + Vector2(1, -1),
 								pos + Vector2(-1, 1), pos + Vector2(1, 1) ])
 	var count = 0
+	var mas = []
 	for point in points:
 		var x = point.x
 		var y = point.y
 		if (x < 0 || x > map_node.map_size.x/global.map_node.ts - 1) || (y < 0 || y > map_node.map_size.y/global.map_node.ts - 1):
 			count += 1
-		if map_node.calc_point_index(point) > pathfinder_node.grid.size():
+			continue
+		if map_node.calc_point_index(point) > pathfinder_node.grid.size()-1:
 			count += 1
-		if !pathfinder_node.grid[map_node.calc_point_index(point)].open && pathfinder_node.grid[map_node.calc_point_index(point)] != cell:
+			continue
+		if (!pathfinder_node.grid[map_node.calc_point_index(point)].open && pathfinder_node.grid[map_node.calc_point_index(point)].wall == null) && pathfinder_node.grid[map_node.calc_point_index(point)] != cell:
 			count += 1
-	if count == points.size():
-		blocked = true
-	else:
-		blocked = false
+			continue
+		if pathfinder_node.grid[map_node.calc_point_index(point)] == cell:
+			count += 1
+			continue
+		if closedCells.has(point):
+			count += 1
+			continue
+		if flag:
+			mas.append(point)
+	if flag:
+		return mas
+	elif count == points.size():
+		return true
+	return false
+
+
+
