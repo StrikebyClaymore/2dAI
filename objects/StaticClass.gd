b4684c9b2ae68a3d1b899c36e1a83f60c7563148
StrikebyClaymore <36325557+StrikebyClaymore@users.noreply.github.com>
2018-12-19 10:01:22 +0300

    Update StaticClass.gd

--- objects/StaticClass.gd
+++ objects/StaticClass.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var idx
 var open = true
+var cost = 1
+var mob = null
 
 func _ready():
 	connect("input_event", self, "_on_body_input_event")
@@ -11,15 +13,18 @@ func _on_body_input_event(viewport, event, shape_idx):
 		if Input.is_action_pressed("r_click"):
 			global.player.position = position
 			open = false
+			mob = global.player
 			global.cell.open = true
+			global.cell.mob = null
 			global.cell = self
 			#global.CheckBlocked()
-			global.flag = false
+			#global.flag = false
 			global.UnlockEnemies()
 		elif Input.is_action_pressed("l_click"):
 			global.map_node.create_enemy(position, self)
 		elif Input.is_action_pressed("middle_click"):
 			global.map_node.add_tile(position, 2)
 		elif Input.is_action_pressed("ui_select"):
-			global.pathfinder_node.FindLockedPath(position, global.player.position, self)
+			open = false
+			#global.pathfinder_node.FindLockedPath(position, global.player.position, self))
 			pass
